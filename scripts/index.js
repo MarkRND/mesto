2549a074fa889ccdf51032ef8eef86bf8c353198
Gorlov Mark <masmik@rambler.ru>
2023-01-09 14:01:57 +0300

    add rev2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 let closeBtn = document.querySelector(".popup__close-button");
-let saveBtn = document.querySelector(".popup__save-button");
 let formElement = document.querySelector(".popup__form");
 let popup = document.querySelector(".popup");
-let nameUser = formElement.querySelector(".popup__input-name");
-let professionUser = formElement.querySelector(".popup__input-profession");
+let nameUser = formElement.querySelector(".popup__input_name");
+let professionUser = formElement.querySelector(".popup__input_profession");
 let nameArea = document.querySelector(".profile__name");
 let jobArea = document.querySelector(".profile__profession");
 let editBtn = document.querySelector(".profile__edit-button");
@@ -19,12 +18,12 @@ function popupHide() {
 }
 editBtn.addEventListener("click", popupOpen);
 closeBtn.addEventListener("click", popupHide);
-saveBtn.addEventListener("click", popupHide);
 
 function FormSubmit(evt) {
   evt.preventDefault();
   nameArea.textContent = nameUser.value;
   jobArea.textContent = professionUser.value;
+  popupHide();
 }
 
 formElement.addEventListener("submit", FormSubmit);
